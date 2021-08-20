f1efb02417b68f01724eb13ff59216d99290c216
aiazbekov33 <aiazbekov34@gmail.com>
2021-08-20 19:56:40 +0600

    Dzday2

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+//Задача №1
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите имя: ");
+//        String name = in.nextLine();
+//        System.out.print("Введите возраст: ");
+//        int age = in.nextInt();
+//        System.out.println("Введите тепературу: ");
+//
+//        int temp = in.nextInt();
+//
+//        if (temp == 20)
+//            System.out.println("Можно гулять, температура: " + temp);
+//        else if (temp == 10)
+//                System.out.println("Надеть свитер прохладно, температура:  " + temp);
+//        else if (temp == 0)
+//                System.out.println("Желательно остаться дома, температура: " + temp);
+//        else
+//                System.out.println("Оставайтесь дома, температура: ");
+//
+//        System.out.println("Имя: "+ name);
+//
+//        System.out.print("Возраст: " + age);
+
+// Зачада №2
+                String Word = "one";
+
+        switch (Word){
+            case "two":
+                System.out.println("Это число 1");
+                break;
+            case 2:
+                System.out.println("Это число 2");
+                break;
+            case 3:
+                System.out.println("Это число 3");
+                break;
+            default:
+                System.out.println("Нет такого числа!!!");
+
+
+
+
+//    }
+//    public static void printText() {
+//        System.out.println("Bart");
+//        System.out.println("Liza");
+//        System.out.println("Homer");
+//        System.out.println("March");
+//        System.out.println("Maggie");
+
+
+    }
+}
